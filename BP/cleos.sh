722d0ead2414b8bceb5b0c18746e9762f0e15956
lucas556 <56923561@qq.com>
2018-05-28 16:58:11 +0800

    Update cleos.sh

--- BP/cleos.sh
+++ BP/cleos.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-NODEOSBINDIR="/path/to/eos/build/programs"
+NODEOSBINDIR="/home/eos-dawn-v4.2.0/eos/build/programs/"
 
 WALLETHOST="127.0.0.1"
 NODEHOST="127.0.0.1"
 NODEPORT="8888"
 WALLETPORT="8900"
-
 
 
 $NODEOSBINDIR/cleos/cleos -u http://$NODEHOST:$NODEPORT --wallet-url http://$WALLETHOST:$WALLETPORT "$@"
